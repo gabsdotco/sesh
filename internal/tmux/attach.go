09247d5f659bbeb53823474c8c8a28c7539a76f2
gabsdotco <biewxw@gmail.com>
2026-07-28 01:37:47 -0300

    fix: route attach-session through CommandRunner for testability

--- internal/tmux/attach.go
+++ internal/tmux/attach.go
@@ -2,7 +2,6 @@ package tmux
 
 import (
 	"os"
-	"os/exec"
 )
 
 // AttachSession attaches to a tmux session (or switches if already inside tmux)
@@ -11,10 +10,5 @@ func (c *Client) AttachSession(name string) error {
 		return c.run("switch-client", "-t", name)
 	}
 
-	cmd := exec.Command("tmux", "attach-session", "-t", name)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	return cmd.Run()
+	return c.run("attach-session", "-t", name)
 }
--- internal/tmux/client.go
+++ internal/tmux/client.go
@@ -3,6 +3,7 @@ package tmux
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -18,6 +19,9 @@ type RealRunner struct{}
 
 func (r *RealRunner) Run(name string, args ...string) error {
 	cmd := exec.Command(name, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	return cmd.Run()
 }
 
